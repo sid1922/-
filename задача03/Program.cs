3f291d5ceb03cc343d1e2e6a6bc9ca583712c6df
sid1922 <111591366+sid1922@users.noreply.github.com>
2022-09-02 20:21:37 +0300

    задача03

---
+++ задача03/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Введите число");
+
+int var;
+
+var = int.Parse(Console.ReadLine());
+
+if ( var % 2==0)
+{
+    Console.WriteLine ("Число четное");
+}
+else
+{
+    Console.WriteLine ("Число нечетное");
+}
+ 
