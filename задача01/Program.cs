1427cead08e156a762bbecf589ae79ee0e595c8c
sid1922 <111591366+sid1922@users.noreply.github.com>
2022-09-02 19:01:14 +0300

    задача01

---
+++ задача01/Program.cs
@@ -0,0 +1,6 @@
+﻿var a1 = Decimal.Parse(Console.ReadLine());
+            var a2 = Decimal.Parse(Console.ReadLine());
+            if (a1 > a2)
+                Console.WriteLine(a1);
+            else 
+                Console.WriteLine(a2);
